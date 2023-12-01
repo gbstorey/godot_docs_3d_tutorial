df755de75d9292b6c62e77d600413e6fdc124630
gbstorey <gbstorey@asu.edu>
2023-12-01 16:02:56 -0700

    Added orthogonal camera and movement

---
+++ player.gd
@@ -0,0 +1,28 @@
+extends CharacterBody3D
+
+@export var speed = 14
+
+@export var fall_acceleration = 75
+
+var target_velocity = Vector3.ZERO
+
+func _physics_process(delta):
+	var direction = Vector3.ZERO
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_back"):
+		direction.z += 1
+	if Input.is_action_pressed("move_forward"):
+		direction.z -= 1
+	# Normalize 3D vector to ensure speed is constant even with diagonal
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		$Pivot.look_at(position + direction, Vector3.UP)
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+	if not is_on_floor():
+		target_velocity.y = target_velocity.y - (fall_acceleration*delta)
+	velocity = target_velocity
+	move_and_slide()
